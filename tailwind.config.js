49ae54b74a1f5773831fb9e712d28c3d307e876d
DaiyanNDahy <daiyanndahy@gmail.com>
2024-05-03 08:10:11 +0600

    tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  content: ['index.html'],
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
