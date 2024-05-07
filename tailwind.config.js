18a01fc0893b137cefeb583f337bc777577c0e90
DaiyanNDahy <daiyanndahy@gmail.com>
2024-05-07 16:51:53 +0600

    theme configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
+const colors= require('tailwindcss/colors');
 module.exports = {
   mode: "jit",
   purge: ["./public/**/*.{html,js}"],
   darkMode: "media", // or 'media' or 'class'
   theme: {
+    colors:{
+      gray:colors.coolGray,
+      blue:colors.lightBlue
+     },
+    screens:{
+      sm:"540px",
+      md:"768px",
+      lg:"1260px",
+      '4k':"2000px",
+    },
     extend: {},
   },
   variants: {
